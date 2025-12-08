f76af6d9c295c6682107ad73d6d3dd767765b70b
Raissi Anouer <raissianouer@gmail.com>
2025-12-08 23:38:12 +0100

    add register

---
+++ src/main/java/in/anouer/moneymanager/repository/ProfileRepository.java
@@ -0,0 +1,12 @@
+package in.anouer.moneymanager.repository;
+
+import in.anouer.moneymanager.entity.ProfileEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface ProfileRepository extends JpaRepository<ProfileEntity,Long> {
+
+    //select * from tbl_profiles where email = ?
+    Optional<ProfileEntity> findByEmail(String email);
+}
