2796c0edba7b5834b4761ba209022c58b7c903c6
Anouer <raissianouer@gmail.com>
2025-12-23 10:30:52 +0100

    add production profile

--- src/main/java/in/anouer/moneymanager/service/ProfileService.java
+++ src/main/java/in/anouer/moneymanager/service/ProfileService.java
@@ -7,6 +7,7 @@ import in.anouer.moneymanager.entity.ProfileEntity;
 import in.anouer.moneymanager.repository.ProfileRepository;
 import in.anouer.moneymanager.util.JwtUtil;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
@@ -28,12 +29,15 @@ public class ProfileService {
     private final AuthenticationManager authenticationManager;
     private final JwtUtil jwtUtil;
 
+    @Value("${app.activation.url}")
+    private String activationUrl;
+
     public ProfileDTO registerProfile(ProfileDTO profileDTO){
         ProfileEntity newProfile=toEntity(profileDTO);
         newProfile.setActivationToken(UUID.randomUUID().toString());
         newProfile = profileRepository.save(newProfile);
         //send activation email
-        String activationLink="http://localhost:9090/api/v1.0/activate?token="+newProfile.getActivationToken();
+        String activationLink=activationUrl+"/api/v1.0/activate?token="+newProfile.getActivationToken();
         String subject= "Active your money manager acount";
         String body="Click on the following link to activate your account: "+activationLink;
         emailService.sendEmail(newProfile.getEmail(),subject,body);
