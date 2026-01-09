bd5937fa0d08aee9467d932662002174677a986b
Anouer <raissianouer@gmail.com>
2026-01-09 15:17:42 +0100

    add sidebar buttons

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -2,9 +2,11 @@ import React, { useContext } from "react";
 import { AppContext } from "../context/AppContext";
 import { User } from "lucide-react";
 import { SIDE_BAR_DATA } from "../assets/assets";
+import { useNavigate } from "react-router-dom";
 
 const Sidebar = () => {
   const { user } = useContext(AppContext);
+  const navigate = useNavigate();
   return (
     <div className="w-64 h-[calc(100vh-61px)] bg-white border-gray-200/50 p-5 sticky top-[61px] z-20">
       <div className="flex flex-col items-center justify-center gap-3 mt-3 mb-7">
@@ -23,10 +25,12 @@ const Sidebar = () => {
       </div>
       {SIDE_BAR_DATA.map((item, index) => (
         <button
+          onClick={() => navigate(item.path)}
           key={`menu_${index}`}
-          className="w-full flex items-center gap-4 text-[15px] py-3 px-6 rounded-lg mb-3"
+          className="cursor-pointer w-full flex items-center gap-4 text-[15px] py-3 px-6 rounded-lg mb-3"
         >
           <item.icon className="text-gray-500" />
+          {item.label}
         </button>
       ))}
     </div>
